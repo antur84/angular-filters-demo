edb44bff8c14eddb2ab8ae1a9435aebd99da1879
Andreas Turku <andreas.turku@gmail.com>
2022-03-04 23:45:10 +0100

    feat(filter.component) destroy$

--- src/app/filters/filter.component.ts
+++ src/app/filters/filter.component.ts
@@ -1,8 +1,15 @@
-import { Injectable, Input, Provider, Type } from '@angular/core';
+import { Injectable, OnDestroy, Provider, Type } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
-export abstract class FilterComponent {
+export abstract class FilterComponent implements OnDestroy {
+  protected destroy$ = new Subject<void>();
   abstract key: string;
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
 export const provideSelfAsFilterComponent = <T extends Type<FilterComponent>>(
   val: T
