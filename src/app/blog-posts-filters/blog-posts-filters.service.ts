2a5284b163527c3b75471ea8cd02f3f43058901e
Andreas Turku <andreas.turku@gmail.com>
2022-03-05 10:57:06 +0100

    feat(filters.service) TFilterModel
    setup filters.service to receive argument,
    which requires derived classes to implement
    a mapping method from an array of individual
    filter results, to the desired filter model.
    
    More type help is required, the value from
    the individual filters are any for now

--- src/app/blog-posts-filters/blog-posts-filters.service.ts
+++ src/app/blog-posts-filters/blog-posts-filters.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { map, OperatorFunction } from 'rxjs';
+import { FilterComponentConfig } from '../filters/filter.component';
 import { FiltersService } from '../filters/filters.service';
 
 @Injectable()
-export class BlogPostsFiltersService extends FiltersService {
+export class BlogPostsFiltersService extends FiltersService<BlogPostsFilterModel> {
+  mapToFilterModel: OperatorFunction<
+    { key: string; value: any }[],
+    BlogPostsFilterModel
+  > = map((val) => {
+    const filterConfigs = Object.entries(blogPostsFiltersConfig);
+    return val.reduce((prev, curr) => {
+      const config = filterConfigs.find(
+        ([_, filterConfig]) => filterConfig.key === curr.key
+      );
+      if (!config) {
+        return prev;
+      }
+      const [propertyName] = config;
+      return { ...prev, [propertyName]: curr.value };
+    }, {});
+  });
+
   constructor() {
     super();
   }
--- src/app/filters/filters.service.ts
+++ src/app/filters/filters.service.ts
@@ -1,4 +1,12 @@
 import { Injectable, Provider, Type } from '@angular/core';
+import { OperatorFunction } from 'rxjs';
+@Injectable()
+export abstract class FiltersService<TFilterModel = unknown> {
+  abstract mapToFilterModel: OperatorFunction<
+    { key: string; value: any }[],
+    TFilterModel
+  >;
+}
 
 export const provideAsFiltersService = <T extends Type<FiltersService>>(
   val: T
@@ -9,6 +17,3 @@ export const provideAsFiltersService = <T extends Type<FiltersService>>(
     useExisting: val,
   },
 ];
-
-@Injectable()
-export abstract class FiltersService {}
